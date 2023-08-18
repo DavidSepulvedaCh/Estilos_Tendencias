c43ed99776bc459e23e79a6af5d6c45bf249fae8
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2023-08-17 23:30:42 -0500

    Agregando variables de entorno
    Se agregaron variables de entorno en el backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,16 +2,15 @@
 
 const mongoose = require("mongoose");
 const app = require('./app');
+require('dotenv').config();
 const port = 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://<_USER>:<_PASS>@cluster0.a87koto.mongodb.net/<_DBNAME>?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.a87koto.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`)
     .then(() => {
-        console.log("Conexion a la BD Atlass exitosa...");
+        console.log("Conexion a la BD Atlas exitosa...");
         app.listen(port, () => {
-            console.log("Server in port: " + port);
+            console.log("Server en puerto: " + port);
         });
     })
-
-
     .catch(err => console.log(err));
