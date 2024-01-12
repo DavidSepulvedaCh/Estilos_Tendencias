80ce317655b9f875bf64223993425d0238300836
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2024-01-12 18:03:32 -0500

    refractor: edit relative paths in services

--- hairdresser/src/app/services/product.service.ts
+++ hairdresser/src/app/services/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Product } from "../models/product";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from "../../environment/environment";
+import { environment } from "src/environment/environment";
 
 @Injectable()
 export class ProductService {
