6ab61c92ea81819ba496acb7c652a3ed2c4f19b1
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2024-01-12 19:20:17 -0500

    update imports

--- hairdresser/src/app/services/product.service.ts
+++ hairdresser/src/app/services/product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from "@angular/core";
 import { Product } from "../models/product";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from "../../environment/environment";
+import { environment } from '../../environments/environment';
+
 
 @Injectable()
 export class ProductService {
