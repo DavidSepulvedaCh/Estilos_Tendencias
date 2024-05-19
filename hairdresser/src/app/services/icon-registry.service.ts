a4bee7ba0eb1a1a1fcfab93ca44f9f8b79e1e461
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2024-05-19 00:11:09 -0500

    Update icon-registry.service.ts

--- hairdresser/src/app/services/icon-registry.service.ts
+++ hairdresser/src/app/services/icon-registry.service.ts
@@ -16,31 +16,28 @@ export class IconRegistryService {
 
   private registerIcons() {
     const icons = [
-      { name: 'home', url: '../../../../assets/svg/home.svg' },
-      { name: 'users', url: '../../../../assets/svg/users.svg' },
-      { name: 'settings', url: '../../../../assets/svg/settings.svg' },
-      { name: 'products', url: '../../../../assets/svg/store.svg' },
-      { name: 'services', url: '../../../../assets/svg/collage.svg' },
-      { name: 'logout', url: '../../../../assets/svg/logout.svg' },
-      { name: 'save', url: '../../../../assets/svg/save.svg' },
-      { name: 'edit', url: '../../../../assets/svg/edit.svg' },
-      { name: 'delete', url: '../../../../assets/svg/trash.svg' },
-      { name: 'lock-access', url: '../../../../assets/svg/lock-access.svg' },
-      { name: 'user-delete', url: '../../../../assets/svg/user-delete.svg' },
-      { name: 'facebook', url: '../../../../assets/svg/facebook.svg' },
-      { name: 'instagram', url: '../../../../assets/svg/instagram.svg' },
-      { name: 'github', url: '../../../../assets/svg/github.svg' },
+      { name: 'home', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/home.svg' },
+      { name: 'users', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/users.svg' },
+      { name: 'settings', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/settings.svg' },
+      { name: 'products', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/store.svg' },
+      { name: 'services', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/collage.svg' },
+      { name: 'logout', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/logout.svg' },
+      { name: 'save', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/save.svg' },
+      { name: 'edit', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/edit.svg' },
+      { name: 'delete', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/trash.svg' },
+      { name: 'lock-access', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/lock-access.svg' },
+      { name: 'user-delete', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/user-delete.svg' },
+      { name: 'facebook', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/facebook.svg' },
+      { name: 'instagram', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/instagram.svg' },
+      { name: 'github', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/github.svg' },
       {
-        name: 'shopping-cart-filled',
-        url: '../../../../assets/svg/shopping-cart-filled.svg',
+        name: 'shopping-cart-filled', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/shopping-cart-filled.svg',
       },
       {
-        name: 'shopping-cart',
-        url: '../../../../assets/svg/shopping-cart.svg',
+        name: 'shopping-cart', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/shopping-cart.svg',
       },
       {
-        name: 'lock-access-off',
-        url: '../../../../assets/svg/lock-access-off.svg',
+        name: 'lock-access-off', url: 'Estilos_Tendencias/hairdresser/src/assets/svg/lock-access-off.svg',
       },
     ];
 
